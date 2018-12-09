932a583bdd4d1c8902ff8a5399494e8854d20d26
dauminhquan <dauminhquantlu@gmail.com>
2018-12-09 10:11:55 +0700

    DsadsA

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ let db = {};
 db.init = config => {
     const mongoose = require('mongoose');
     const { db: { host, port, name } } = config;
-    const connectionString = `mongodb://${host}:${port}/${name}`;
-    // const connectionString = 'mongodb://dauminhquantlu:Exx5aEKhwq5wbfUY@cluster0-shard-00-00-fjf7b.mongodb.net:27017,cluster0-shard-00-01-fjf7b.mongodb.net:27017,cluster0-shard-00-02-fjf7b.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    // const connectionString = `mongodb://${host}:${port}/${name}`;
+    const connectionString = 'mongodb://dauminhquantlu:Exx5aEKhwq5wbfUY@cluster0-shard-00-00-fjf7b.mongodb.net:27017,cluster0-shard-00-01-fjf7b.mongodb.net:27017,cluster0-shard-00-02-fjf7b.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
     mongoose.connect(connectionString,{ useNewUrlParser: true });
 
     const db = mongoose.connection;
